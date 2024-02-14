9852ac1bd11410dadc9987217fd03f1b9a0cfa6a
rafvmaia <rafvmaia@gmail.com>
2024-02-14 11:50:46 -0300

    Updates
    Sliders
    e outras áreas

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,25 @@ function nextImage(){
 
   document.getElementById("radio"+count).checked = true;
 }
+
+
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+
+	var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+	var index = 0, time = 3000;
+	slides[index].classList.add('active');
+
+	setInterval( () => {
+		slides[index].classList.remove('active');
+		
+		index++;
+		if (index === slides.length) index = 0;
+
+		slides[index].classList.add('active');
+
+	}, time);
+}
